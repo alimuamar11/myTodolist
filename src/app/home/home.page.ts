da60c9b4f9cb9ee5da0bfe9d4e73d50109401cf3
alimuamar11 <alimuamar11@gmail.com>
2021-09-02 21:08:16 +0700

    todolist static data

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,26 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  todoList = [{
+    itemName: 'Coding',
+    itemDueDate: '10-10-2021',
+    itemPriority: 'low',
+    itemCategory: 'Work'
+  },
+  {
+    itemName: 'Traveling',
+    itemDueDate: '12-12-2021',
+    itemPriority: 'high',
+    itemCategory: 'Refreshing'
+  },
+  {
+    itemName: 'Joging',
+    itemDueDate: '12-16-2021',
+    itemPriority: 'middle',
+    itemCategory: 'Mentallity'
+  }
+  ]
+  today: number = Date.now()
+  constructor() { }
 
 }
